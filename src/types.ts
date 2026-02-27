af740c4446b8b41a1f40b47b28e491a7c534d85e
lfisher447-afk <lfisher447@gmail.com>
2026-02-27 15:31:53 -0600

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,18 @@
+export interface Peak {
+  binIndex: number;
+  frequency: number;
+  amplitude: number;
+}
 
+export interface BandState {
+  id: string;
+  name: string;
+  frequency: number;
+  gainDb: number;
+  color: string;
+}
+
+export interface AudioStudio {
+  exportPreset: () => Record<string, unknown>;
+  importPreset: (data: Record<string, unknown>) => void;
+}
